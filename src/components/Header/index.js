35267e46abc6041046f44ab512377350ce799ac1
unknown <rajovicmirko@gmail.com>
2020-10-10 16:15:25 +0200

    Prepared Website for IE11

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,34 +2,27 @@ import React from "react";
 
 function index() {
   return (
-    <section id="home">
-      <header
-        id="#"
-        className="d-flex justify-content-center align-items-center text-white"
-      >
-        <div className="container d-flex flex-column justify-content-center align-items-center">
-          <h1 className="display-3 text-center">Mirko Rajović</h1>
-          <h4>Frontend developer</h4>
-          <div className="contact-list">
-            <a
-              href="https://www.linkedin.com/in/mirko-rajovic-170936158"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mx-3"
-            >
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a
-              href="https://github.com/RajovicMirko"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mx-3"
-            >
-              <i className="fab fa-github-square"></i>
-            </a>
-          </div>
-        </div>
-      </header>
+    <section id="home" className="d-flex flex-column justify-content-center align-items-center text-white">
+      <h1 className="display-3 text-center">Mirko Rajović</h1>
+      <h4>Frontend developer</h4>
+      <div className="contact-list">
+        <a
+          href="https://www.linkedin.com/in/mirko-rajovic-170936158"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mx-3"
+        >
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a
+          href="https://github.com/RajovicMirko"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mx-3"
+        >
+          <i className="fab fa-github-square"></i>
+        </a>
+      </div>
     </section>
   );
 }
